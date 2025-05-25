7a960cd8c0a7d0a49c50a31d81403ebb016ed359
Vinay Kumar J <jvkumar2003@gmail.com>
2025-05-25 17:40:50 +0530

    modified code at css

--- src/Header.jsx
+++ src/Header.jsx
@@ -10,8 +10,7 @@ export function MainHeader() {
 
                     <ul className="header-ul">
 
-                        <li className="logo"><h1>Mrcl</h1></li>
-                        
+                        <li className="logo"><h1>MIRACLE</h1></li>
                         <li className="nav-li"><Link to="/">Home</Link></li>
                         <li className="nav-li"><Link to="/EventContent">Events</Link></li>
                         <li className="nav-li"><Link to="/RegisterContent">Register</Link></li>
