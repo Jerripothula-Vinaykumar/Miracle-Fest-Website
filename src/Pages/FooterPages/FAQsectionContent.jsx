c3f61e6e558e2667f9a689cb1b373d4a15d43a91
Vinay Kumar J <jvkumar2003@gmail.com>
2025-06-15 12:33:19 +0530

    faq section

--- src/Pages/FooterPages/FAQsectionContent.jsx
+++ src/Pages/FooterPages/FAQsectionContent.jsx
@@ -1,6 +1,85 @@
 
 export function FaqSectionContent() {
   return (
-    <main className="faqsectioncontent">Faq Section Content Page Section</main>
+
+    <main className="faqsectioncontent">
+        <div className="faqsectioncontainer">
+
+        <h1 className="faqheader">Frequently Asked Questions</h1>
+       
+        <details className="faqquestioncontainer">
+          <summary className="faqquestion">
+                  <span className="qn">What is Miracle Fest ?</span>
+          </summary>
+          <p className="faqanswer">  Miracle Fest is a annual celebration hosted by Miralce Educational Society Group of Institutions , Featuring Cultural & Sports and Technical Events .</p>
+        </details >
+      
+       <details className="faqquestioncontainer">
+          <summary className="faqquestion">
+                   <span className="qn">Who Can Praticipate in the Fest ? </span>
+          </summary>
+          <p className="faqanswer"> All Students of Miralce Educational Society Group of Institutions , can take part in this Event .</p>
+        
+        </details >
+
+         <details className="faqquestioncontainer">
+          <summary className="faqquestion">
+                   <span className="qn">How do I Register For an Event ?</span>
+          </summary>
+          <p className="faqanswer">  Visit  the Event Registration Page , Choose your Event , Fill the details and submit the form to register .</p>
+        </details >
+
+         <details className="faqquestioncontainer">
+          <summary className="faqquestion">
+                   <span className="qn">Is there a Registration Fee ?</span>
+          </summary>
+          <p className="faqanswer"> Some Events are Free , Others may require a Nominal Fee , Check the Event details  for Specific Fee information . </p>
+        </details >
+
+         <details className="faqquestioncontainer">
+          <summary className="faqquestion">
+                    <span className="qn">Can I Register For Multiple Events ?</span>
+          </summary>
+          <p className="faqanswer"> Yes , you can Register For Multiple Events as Long as their timings don`t Clash .  </p>
+        </details >
+
+         <details className="faqquestioncontainer">
+          <summary className="faqquestion">
+                  <span className="qn"> What are available Payment Methods ?</span>
+          </summary>
+          <p className="faqanswer"> Payments can be made Online via Phonepay usign UPI , or pay offline to the Coordinator  .</p>
+        </details >
+
+         <details className="faqquestioncontainer">
+          <summary className="faqquestion">
+                 <span className="qn">How do I create an Account ?</span>
+          </summary>
+          <p className="faqanswer"> Click on the Sign-up button , fill in your details , and verify your email and password to complete account creation .</p>
+        </details >
+
+         <details className="faqquestioncontainer">
+          <summary className="faqquestion">
+                   <span className="qn">I forgot My Password , What should I do ?</span>
+          </summary>
+          <p className="faqanswer"> Click on "Forgot Password" on the login page and follow the Instructions to reset it securely .</p>
+        </details >
+      
+         <details className="faqquestioncontainer">
+          <summary className="faqquestion">
+                   <span className="qn">How do I Know the event timing and Venue ? </span>
+          </summary>
+          <p className="faqanswer">Each event card shows the Event date and start time , end time , and Venue In the Event Page .  </p>
+        </details > 
+
+         <details className="faqquestioncontainer">
+          <summary className="faqquestion">
+                   <span className="qn">How can I Contact Fest Organizers  ? </span>
+          </summary>
+          <p className="faqanswer"> Visit the Support&QA Page or Contact the listed Event Coordinators under Each Event For any Queries . </p>
+        </details >       
+            
+            </div>
+
+      </main>
   );
 }
