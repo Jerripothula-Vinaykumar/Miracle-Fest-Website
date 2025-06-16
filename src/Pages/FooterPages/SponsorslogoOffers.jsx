79388f2110da6782d874621b31168735ef439e0a
Vinay Kumar J <jvkumar2003@gmail.com>
2025-06-16 21:36:57 +0530

    volunteer page

--- src/Pages/FooterPages/SponsorslogoOffers.jsx
+++ src/Pages/FooterPages/SponsorslogoOffers.jsx
@@ -2,11 +2,11 @@ export function SponsorslogosandoffersContent() {
   return (
     <main className="sponsorslogosandofferscontent">
       <div className="sponserlogoheader">Sponsers Logos </div>
-      <div className="logosflexcontainer"><div className="logoflexitems"></div></div>
+      <div className="logosflexcontainer"><div className="logoflexitems">Banner</div></div>
       <div className="logosgridcontainer">
-        <div className="logogriditems"></div>
-        <div className="logogriditems"></div>
-        <div className="logogriditems"></div>
+        <div className="logogriditems">Image</div>
+        <div className="logogriditems">Image</div>
+        <div className="logogriditems">Image</div>
       </div>
     </main>
   );
--- src/Pages/FooterPages/VolunteersAssignment.jsx
+++ src/Pages/FooterPages/VolunteersAssignment.jsx
@@ -2,8 +2,31 @@
 export function VolunteersAssignmentContent() {
   return (
     <main className="volunteersassignmentcontent">
-      <div className="volunteersassignment-container">
-          Volunteers Assignment Content Page Section
+      <div className="volunteersassignmentcontainer">
+        
+        <div className="volunteerpageheadercontainer">
+          <h1 className="vpeventname">Event Name </h1>
+          <h1 className="vpeventcoordinator">Assigned Coordinator </h1>
+          <h1 className="vpeventcoordinatorcontact">Contact </h1>
+        
+          </div>      
+        <div className="volunteercontact"> <span className=""> Banner Launch</span>  <span> 1 . k . Srinivasa Rao [ DEAN ]</span> <span>00000-00000</span></div>
+        <div className="volunteercontact"> <span> Traditional Day</span> 2 . A . Arjuna Rao [ PRINCIPAL ] <span>00000-00000</span></div>
+        <div className="volunteercontact"> <span> Final Fest </span> 3 . CSE HOD <span>00000-00000</span></div>
+        <div className="volunteercontact"> <span>Pro coder</span> 4 . ECE HOD <span>00000-00000</span></div>
+        <div className="volunteercontact"> <span>Guess The Output</span> 5 . EEE HOD <span>00000-00000</span></div>
+        <div className="volunteercontact"> <span>Quiz</span> 6 . Mechanical HOD <span>00000-00000</span></div>
+        <div className="volunteercontact"> <span>Essay Writing</span> 7 . Fest Committe Head <span>00000-00000</span></div>
+        <div className="volunteercontact"> <span>Men`s Cricket </span> 8 . Disciplane Committe Head <span>00000-00000</span></div>
+        <div className="volunteercontact"> <span>Men`s Kabaddi</span> 9 . Main Coordinator <span>00000-00000</span></div>
+        <div className="volunteercontact"> <span>Men`s Volley Ball </span> 5 . EEE HOD <span>00000-00000</span></div>
+        <div className="volunteercontact"> <span>Men`s Kho Kho </span> 6 . Mechanical HOD <span>00000-00000</span></div>
+        <div className="volunteercontact"> <span>Carrom Board</span> 7 . Fest Committe Head <span>00000-00000</span></div>
+        <div className="volunteercontact"> <span>Free Fire</span> 8 . Disciplane Committe Head <span>00000-00000</span></div>
+        <div className="volunteercontact"> <span>BGMI</span> 9 . Main Coordinator <span>00000-00000</span></div>
+        <div className="volunteercontact"> <span>Women`s Throw Ball</span> 9 . Main Coordinator <span>00000-00000</span></div>
+
+  
       </div>
      
     </main>
