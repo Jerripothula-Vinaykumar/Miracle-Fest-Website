a4dfac753d2222d87a236c78975c2bc44245846c
Vinay Kumar J <jvkumar2003@gmail.com>
2025-06-16 16:46:36 +0530

    emergency contacts page

--- src/Pages/FooterPages/EmergencyContent.jsx
+++ src/Pages/FooterPages/EmergencyContent.jsx
@@ -2,7 +2,19 @@
 export function EmergencyContactContent() {
   return (
     <main className="emergencycontactcontent">
-      Emergency Contact Content Page Section
+      <div className="emergencycontactcontainer">
+        <h1 className="emergencycontactsheader">Emergency Contacts</h1>
+        <div className="emergencycontact"> 1 . k . Srinivasa Rao [ DEAN ] <span>00000-00000</span></div>
+        <div className="emergencycontact"> 2 . A . Arjuna Rao [ PRINCIPAL ] <span>00000-00000</span></div>
+        <div className="emergencycontact"> 3 . CSE HOD <span>00000-00000</span></div>
+        <div className="emergencycontact"> 4 . ECE HOD <span>00000-00000</span></div>
+        <div className="emergencycontact"> 5 . EEE HOD <span>00000-00000</span></div>
+        <div className="emergencycontact"> 6 . Mechanical HOD <span>00000-00000</span></div>
+        <div className="emergencycontact"> 7 . Fest Committe Head <span>00000-00000</span></div>
+        <div className="emergencycontact"> 8 . Disciplane Committe Head <span>00000-00000</span></div>
+        <div className="emergencycontact"> 9 . Main Coordinator <span>00000-00000</span></div>
+      
+      </div>
     </main>
   );
 }
