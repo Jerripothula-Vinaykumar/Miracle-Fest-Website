75d8bed26b8e199c0b63204eb7494ba7f2cb3c09
Vinay Kumar J <jvkumar2003@gmail.com>
2025-06-16 12:50:17 +0530

    feedback page

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -126,7 +126,7 @@ export function Footer() {
               </li>
               <li>
                 <Link to="/CertificateDownloadContent">
-                  Certificate Download
+                  Album Download
                 </Link>
               </li>
               </div>
--- src/Pages/FooterPages/CertificateDownload.jsx
+++ src/Pages/FooterPages/CertificateDownload.jsx
@@ -2,13 +2,13 @@
 export function CertificateDownloadContent() {
   return (
     <main className="certificatedownloadcontent">
-      <div className="certificatedownloadheader"> Here is your Certificate , user</div>
+      <div className="certificatedownloadheader"> Here is your College Album , user</div>
       <div className="certificatedownloadbox">
 
        <div className="samplecertificate">
-        Preview of Certificate
+        Preview of Album
         </div>
-        <button type="download" className="downloadcertificatebutton">Download Certificate</button>
+        <button type="download" className="downloadcertificatebutton">Download</button>
         </div>
     </main>
   );
