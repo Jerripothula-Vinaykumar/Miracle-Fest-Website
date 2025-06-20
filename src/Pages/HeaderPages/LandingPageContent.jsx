ecf0f4b79b98f87bed3233456257a682d8b9c2c4
Vinay Kumar J <jvkumar2003@gmail.com>
2025-06-20 10:48:57 +0530

    landing page in laptop

--- src/Pages/HeaderPages/LandingPageContent.jsx
+++ src/Pages/HeaderPages/LandingPageContent.jsx
@@ -1,15 +1,100 @@
-
+import { Link } from "react-router-dom"
 
 export default function LandingPageContent() {
     
     return (
         
         <main className="landingpagecontent">
-        
-       
-         <div className="countdowncontainer">
-            Landing Page
+          <div className="landingpagecontainer">
+            <div className="landingpageflexcontainer">
+                <div className="landingpagegriditem1">
+                  <h1 className="landingpagegriditem1header">
+                    WELCOME ,  <br />TO THE MIRACLE FEST
+                    </h1>
+                  <details className="landingpagedetailscontainer">
+                    <summary className="landingpagedetailsqn">
+                            Question no 1 . jdddddddddddajda davvvvvvvvv  {">"}
+                    </summary>
+                    <p className="landingpagedetailsanswer">
+                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus fuga cum ab ducimus voluptate reiciendis veritatis sit harum aut officia, dolorum inventore nam quo adipisci dolores molestiae aperiam quam sint.
+                         Sed laboriosam aliquid vel sint natus, exercitationem quia vero eligendi, iste, molestiae nostrum atque error nam aliquam itaque quibusdam. Quasi tempora libero illo ad reiciendis excepturi perferendis, aliquam quod voluptatem?
+                         Cupiditate veniam quidem animi illo ullam, quae, nisi ratione eveniet incidunt, perspiciatis corrupti natus deserunt alias. Earum alias nam neque autem suscipit laboriosam, rem cupiditate veniam quod doloribus mollitia vero!
+                    </p>
+                     
+                    </details>
+                  
+                    <details className="landingpagedetailscontainer">
+                    <summary className="landingpagedetailsqn">
+                            Question no 1 . jdddddddddddajda davvvvvvvvv  {">"}
+                    </summary>
+                    <p className="landingpagedetailsanswer">
+                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus fuga cum ab ducimus voluptate reiciendis veritatis sit harum aut officia, dolorum inventore nam quo adipisci dolores molestiae aperiam quam sint.
+                         Sed laboriosam aliquid vel sint natus, exercitationem quia vero eligendi, iste, molestiae nostrum atque error nam aliquam itaque quibusdam. Quasi tempora libero illo ad reiciendis excepturi perferendis, aliquam quod voluptatem?
+                         Cupiditate veniam quidem animi illo ullam, quae, nisi ratione eveniet incidunt, perspiciatis corrupti natus deserunt alias. Earum alias nam neque autem suscipit laboriosam, rem cupiditate veniam quod doloribus mollitia vero!
+                    </p>
+                     
+                    </details>
+                    <details className="landingpagedetailscontainer">
+                    <summary className="landingpagedetailsqn">
+                            Question no 1 . jdddddddddddajda davvvvvvvvv  {">"}
+                    </summary>
+                    <p className="landingpagedetailsanswer">
+                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus fuga cum ab ducimus voluptate reiciendis veritatis sit harum aut officia, dolorum inventore nam quo adipisci dolores molestiae aperiam quam sint.
+                         Sed laboriosam aliquid vel sint natus, exercitationem quia vero eligendi, iste, molestiae nostrum atque error nam aliquam itaque quibusdam. Quasi tempora libero illo ad reiciendis excepturi perferendis, aliquam quod voluptatem?
+                         Cupiditate veniam quidem animi illo ullam, quae, nisi ratione eveniet incidunt, perspiciatis corrupti natus deserunt alias. Earum alias nam neque autem suscipit laboriosam, rem cupiditate veniam quod doloribus mollitia vero!
+                    </p>
+                     
+                    </details>
+                    <details className="landingpagedetailscontainer">
+                    <summary className="landingpagedetailsqn">
+                            Question no 1 . jdddddddddddajda davvvvvvvvv  {">"}
+                    </summary>
+                    <p className="landingpagedetailsanswer">
+                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus fuga cum ab ducimus voluptate reiciendis veritatis sit harum aut officia, dolorum inventore nam quo adipisci dolores molestiae aperiam quam sint.
+                         Sed laboriosam aliquid vel sint natus, exercitationem quia vero eligendi, iste, molestiae nostrum atque error nam aliquam itaque quibusdam. Quasi tempora libero illo ad reiciendis excepturi perferendis, aliquam quod voluptatem?
+                         Cupiditate veniam quidem animi illo ullam, quae, nisi ratione eveniet incidunt, perspiciatis corrupti natus deserunt alias. Earum alias nam neque autem suscipit laboriosam, rem cupiditate veniam quod doloribus mollitia vero!
+                    </p>
+                     
+                    </details>
+                    <details className="landingpagedetailscontainer">
+                    <summary className="landingpagedetailsqn">
+                            Question no 1 . jdddddddddddajda davvvvvvvvv  {">"}
+                    </summary>
+                    <p className="landingpagedetailsanswer">
+                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus fuga cum ab ducimus voluptate reiciendis veritatis sit harum aut officia, dolorum inventore nam quo adipisci dolores molestiae aperiam quam sint.
+                         Sed laboriosam aliquid vel sint natus, exercitationem quia vero eligendi, iste, molestiae nostrum atque error nam aliquam itaque quibusdam. Quasi tempora libero illo ad reiciendis excepturi perferendis, aliquam quod voluptatem?
+                         Cupiditate veniam quidem animi illo ullam, quae, nisi ratione eveniet incidunt, perspiciatis corrupti natus deserunt alias. Earum alias nam neque autem suscipit laboriosam, rem cupiditate veniam quod doloribus mollitia vero!
+                    </p>
+                     
+                    </details>
+                    <details className="landingpagedetailscontainer">
+                    <summary className="landingpagedetailsqn">
+                            Question no 1 . jdddddddddddajda davvvvvvvvv  {">"}
+                    </summary>
+                    <p className="landingpagedetailsanswer">
+                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus fuga cum ab ducimus voluptate reiciendis veritatis sit harum aut officia, dolorum inventore nam quo adipisci dolores molestiae aperiam quam sint.
+                         Sed laboriosam aliquid vel sint natus, exercitationem quia vero eligendi, iste, molestiae nostrum atque error nam aliquam itaque quibusdam. Quasi tempora libero illo ad reiciendis excepturi perferendis, aliquam quod voluptatem?
+                         Cupiditate veniam quidem animi illo ullam, quae, nisi ratione eveniet incidunt, perspiciatis corrupti natus deserunt alias. Earum alias nam neque autem suscipit laboriosam, rem cupiditate veniam quod doloribus mollitia vero!
+                    </p>
+                     
+                    </details>
+
+
+                </div>
+                <div className="landingpagegriditem2">
+                  <div className="landingpageclgimg">
+                  </div>
+
+                </div>
+                </div>
+                <div className="landingpageloginbuttonitem">
+                    <h1 className="landingpageloginbuttonincontent">Want to Raom Around the Fest ,  </h1>
+                     <button className="landingregisterincontent"><Link to="/RegisterContent">Login</Link></button>
+                </div>
+                
           </div>
+       
+         
 
         </main>
     )
